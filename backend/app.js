49e4cf349ce51ce9ef10b959ba59cfd37ddb0d3b
Abishalini1602 <abiabishalini1002@gmail.com>
2025-06-14 17:16:11 +0530

    Add proper CORS config for frontend

--- backend/app.js
+++ backend/app.js
@@ -8,18 +8,23 @@ const UserAPI = require("./router/user");
 const TaskAPI = require("./router/task");
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "https://task-manage-mern.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/api/v1", UserAPI);
 app.use("/api/v2", TaskAPI);
 
 app.use("/", (req, res) => {
-    res.send("Hello from the backend!");
+  res.send("Hello from the backend!");
 });
 
-//localhost:1000/api/v1/sign-in
 const PORT = 1000;
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
